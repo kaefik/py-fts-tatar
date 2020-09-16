ef8d61bed105fb9273accbc4c909c67614a0092d
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-09-16 17:44:22 +0300

    добвлено разделение одного файла дампа в отдельные файлы

--- split_wiki_dump.py
+++ split_wiki_dump.py
@@ -1,6 +1,10 @@
 import xml.etree.ElementTree as ET
+import os
 
 filename = 'data/ttwiki-20200901-pages-articles-multistream.xml'
+out_folder = 'data/out/'
+
+kol_files_in_folder = 10000
 
 # tree = ET.parse(filename)
 # root = tree.getroot()
@@ -11,7 +15,27 @@ filename = 'data/ttwiki-20200901-pages-articles-multistream.xml'
 #
 # print(tag.attrib())
 
+number_file = 1
+number_folder = 1
+page_item = ""
+os.mkdir(f"{out_folder}{number_folder}")
 with open(filename, 'r') as f:
-    for line in f:
-        line = line.strip()
-        print(line)
+    for xmlline in f:
+        line = xmlline.strip()
+        page_item += xmlline
+        # print(line)
+
+        if (len(line)>0) and (line in '</page>'):  # конец статьи+
+            print(f'Detect new page: {number_file}')
+
+            out_filename = f'{out_folder}{number_folder}/{number_file}.xml'
+            print(line)
+            with open(out_filename, 'w') as f_out:
+                f_out.write(page_item)
+            page_item = ''
+            number_file += 1
+
+        if number_file > kol_files_in_folder:
+            number_file = 1
+            number_folder += 1
+            os.mkdir(f"{out_folder}{number_folder}")
