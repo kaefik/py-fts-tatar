013dc6aabcc8c96f1697bf6abacb001e72c5bf28
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-09-21 15:25:25 +0300

    вывел разделение дампа на отдельные файлы в отдельную функцию

--- split_wiki_dump.py
+++ split_wiki_dump.py
@@ -1,41 +1,42 @@
-import xml.etree.ElementTree as ET
+'''
+    разделение дампа вики на отдельные статьи.
+'''
 import os
 
-filename = 'data/ttwiki-20200901-pages-articles-multistream.xml'
-out_folder = 'data/out/'
 
-kol_files_in_folder = 10000
+def split_wiki(input_filename, out_folder='data/out/', kol_files_in_folder=10000):
+    """
+        :param input_filename: путь до дампа википедии (формат xml)
+        :param out_folder: путь куда сохраняются статьи выделенные из дампа
+        :param kol_files_in_folder: количество статей в папке
+        :return: True, если все прошло хорошо
+    """
+    number_file = 1
+    number_folder = 1
+    page_item = ''
+    os.mkdir(f"{out_folder}{number_folder}")
+    with open(input_filename, 'r') as f:
+        for xmlline in f:
+            line = xmlline.strip()
+            page_item += xmlline
 
-# tree = ET.parse(filename)
-# root = tree.getroot()
-#
-# tag = root.tag
-#
-# print(tag)
-#
-# print(tag.attrib())
+            if (len(line) > 0) and (line in '</page>'):  # конец статьи
+                print(f'Detect new article: {number_file}')
 
-number_file = 1
-number_folder = 1
-page_item = ""
-os.mkdir(f"{out_folder}{number_folder}")
-with open(filename, 'r') as f:
-    for xmlline in f:
-        line = xmlline.strip()
-        page_item += xmlline
-        # print(line)
+                out_filename = f'{out_folder}{number_folder}/{number_file}.xml'
+                # print(line)
+                with open(out_filename, 'w') as f_out:
+                    f_out.write(page_item)
+                page_item = ''
+                number_file += 1
 
-        if (len(line)>0) and (line in '</page>'):  # конец статьи+
-            print(f'Detect new page: {number_file}')
+            if number_file > kol_files_in_folder:
+                number_file = 1
+                number_folder += 1
+                os.mkdir(f"{out_folder}{number_folder}")
+    return True
 
-            out_filename = f'{out_folder}{number_folder}/{number_file}.xml'
-            print(line)
-            with open(out_filename, 'w') as f_out:
-                f_out.write(page_item)
-            page_item = ''
-            number_file += 1
 
-        if number_file > kol_files_in_folder:
-            number_file = 1
-            number_folder += 1
-            os.mkdir(f"{out_folder}{number_folder}")
+if __name__ == '__main__':
+    filename = 'data/ttwiki-20200901-pages-articles-multistream.xml'
+    split_wiki(filename)
